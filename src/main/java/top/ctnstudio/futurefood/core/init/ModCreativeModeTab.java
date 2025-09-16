a2b755301d0b637cad308995d218a687d48173ac
AmarokIce <snowlyicewolf@outlook.com>
2025-09-16 20:06:52 +0800

    🐺📨

--- src/main/java/top/ctnstudio/futurefood/core/init/ModCreativeModeTab.java
+++ src/main/java/top/ctnstudio/futurefood/core/init/ModCreativeModeTab.java
@@ -4,6 +4,7 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.RegisterEvent;
 import top.ctnstudio.futurefood.core.FutureFood;
 
